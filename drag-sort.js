f69dfe737931977e7dd923bc9b254ffbac6b39ee
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2019-12-12 11:46:48 +0800

    First commit

---
+++ drag-sort.js
@@ -0,0 +1,41 @@
+/***********************************************************************
+ *
+ *   DRAG FEATURE
+ *
+ ***********************************************************************/
+
+const sortables = Array.from(document.querySelectorAll(".sortable"));
+
+// Create array for the z-indexes
+// Initializes the z-indexes of the elements in sortables
+const zIndex = [];
+for (let i = 1; i <= sortables.length; i++) {
+    zIndex[i - 1] = i * 10;
+    sortables[i - 1].style.zIndex = Number(zIndex[i - 1]);
+}
+
+// z-index value for front most layer
+const frontLayer = zIndex.slice(-1)[0];
+
+
+for (let i = 0; i < sortables.length; i++) {
+    sortables[i].addEventListener("mousedown", bringForward);
+}
+
+
+function bringForward(e) {
+    let currentLayer = e.target.style.zIndex;
+
+    sortables.forEach(element => {
+        /* Only have to sort elements in front of targeted element */
+        if (element.style.zIndex > currentLayer) {
+            element.style.zIndex -= 10;
+        }
+    });
+    
+    e.target.style.zIndex = frontLayer;
+}
+
+$(".drag").draggable({
+    containment: "parent"
+});
---
+++ script.js
@@ -0,0 +1,29 @@
+const roles = document.querySelector("#roles")
+const roleArray = document.querySelectorAll(".role");
+const descriptions = [
+    ["creative", "communication", "practitioner"],
+    ["damn", "annoying", "brother"],
+    ["very", "indecisive", "dresser"]
+];
+
+let iDescription = 0;
+
+function cycleRoles(index) {
+    let nextIndex = index;
+
+    for (let i = 0; i < roleArray.length; i++) {
+        roleArray[i].innerHTML = descriptions[index][i];
+    }
+
+    /* i should return the next index value to use in description array */
+    nextIndex++;
+
+    /* reset iDescription so index doesn't go out of bounds */
+    if (nextIndex == descriptions.length)
+        nextIndex = 0;
+
+    iDescription = nextIndex;
+}
+
+roles.addEventListener("click", () => cycleRoles(iDescription));
+
