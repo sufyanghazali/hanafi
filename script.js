fc81674739194a7e238f9e9b45ebb32a64d7fd51
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2019-12-19 11:04:10 +0800

    Changes

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.addEventListener("scroll", () => {
 
 
 // const dropdown = document.querySelectorAll(".crumb::after");
-const crumbs = document.querySelectorAll(".crumb");
+const dropdowns = document.querySelectorAll(".drop-wrap");
 
 function newDiv() {
     return document.createElement("div");
@@ -71,29 +71,37 @@ function createArrow() {
 }
 
 
-for (let i = 0; i < crumbs.length; i++) {
-    let parent = crumbs[i].parentNode;
+for (let i = 0; i < dropdowns.length; i++) {
+    let parent = dropdowns[i];
     newArrow = createArrow();
-    parent.insertBefore(newArrow, crumbs[i].nextSibling);
+    parent.insertBefore(newArrow, parent.firstElementChild.nextSibling);
 }
 
 const arrows = document.querySelectorAll(".arrow");
 
 for (let i = 0; i < arrows.length; i++) {
+
     arrows[i].addEventListener("click", () => {
-        arrows[i].nextElementSibling.classList.toggle("drop");
-    });
+        let dd = arrows.nextElementSibling
+        if (typeof (dd) != "undefined" && dd != null) {
+            if (dd.id = "") {
+                dd.id = "drop"
+            } else {
+                dd.id = "";
+            }
+        }
+    }); 
 }
 
 html.addEventListener("click", e => {
-    let drop = document.querySelector(".drop");
-    
+    let drop = document.querySelector("#drop");
+
     // only runs if a dropdown is active
     if (typeof (drop) != "undefined" && drop != null) {
 
         // cannot have clicked on the dropdown itself or the arrow (because it'll close it straightaway)
         if (!drop.contains(e.target) && !e.target.classList.contains("arrow")) {
-            drop.classList.remove("drop");
+            drop.id = "";
         }
     }
 });
