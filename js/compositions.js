a8f14be4079c5d5f88e03ae2c5488f999fdb9db6
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2019-12-30 20:31:30 +0800

    Implemented directional resize for compositions page

---
+++ js/compositions.js
@@ -0,0 +1,24 @@
+const imageContainers = document.querySelectorAll(".composition-container");
+const halfway = (document.querySelector(".canvas").clientWidth) / 2
+
+// add event listener to all images
+// on click
+// get position where the middle of the image
+// if position is over half scale to left
+// if position is on half, scale in position
+// if position is less than half, scale to right
+
+imageContainers.forEach(image => {
+    let middle = image.offsetLeft + image.offsetWidth / 2;
+    console.log(middle);
+    console.log(halfway);
+    if (middle > halfway) {
+        image.classList.add("scale-left");
+    } else if (middle < halfway) {
+        image.classList.add("scale-right");
+    }
+
+    image.onclick = function () {
+        this.classList.toggle("scale");
+    }
+});
