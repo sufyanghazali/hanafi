bf8acbaf78ea04ffe070df7326ef33821c7fe3fd
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2020-01-27 19:28:07 +0800

    started temp revamp

--- js/temperature.js
+++ js/temperature.js
@@ -16,6 +16,8 @@ left: ${left}px;
 `;
 }
 
+
+
 function getValues()
 {
     let str = "";
@@ -32,23 +34,66 @@ function getValues()
     console.log(str);
 }
 
+
+function adjust(image)
+{
+    const width = image.offsetWidth;
+    const top = image.offsetTop;
+    const left = image.offsetLeft;
+
+    return `#${image.id} {
+width: ${width}px;
+top: ${top + 150}px;
+left: ${left}px;
+}
+
+`;
+}
+
+function getAdjusted()
+{
+    let str = "";
+
+    temps.forEach((image =>
+    {
+        str += adjust(image);
+    }));
+
+    console.log(str);
+}
+
 temps.forEach(image =>
 {
     image.classList.add("drag");
 });
-lines.forEach(line =>
-{
-    line.classList.add("drag");
-});
+// lines.forEach(line =>
+// {
+//     line.classList.add("drag");
+// });
 
 
-$(".drag").draggable({
-    containment: "parent"
-});
+// $(".drag").draggable({
+//     containment: "parent"
+// });
 
 
 /***********************************************************************
- *  
- *  RESIZE FEATURE
- * 
+ *
+ *  move-slider
+ *
  **********************************************************************/
+
+const temperature = document.querySelector(".temperature");
+const slide = temperature.querySelector(".slide__wrapper");
+
+slide.style.top = "0px";
+
+temperature.addEventListener("click", function ()
+{
+    console.log("clicked");
+    console.log(slide.style.top);
+    const offset = Number(slide.style.top.slice(0, -2));
+    slide.style.top = offset + 50 + "px";
+
+});
+
